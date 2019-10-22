14c1fab329b986c887aad8f81a8fa6608feaf37c
Anand Patel <52890488+devtechnocode@users.noreply.github.com>
2019-10-23 00:50:38 +0530

    Create team.cpp

---
+++ team.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n ,petya ,vasya ,tonya;
+    int counter=0;
+    cin >> n;
+    for (int i=0 ; i<n; i++)
+    {
+        cin >> petya >> vasya >> tonya;
+        if (petya+vasya+tonya>=2)
+        {
+            counter+=1;
+        }
+       
+    }
+    cout<<counter;
+    return 0;     
+}
